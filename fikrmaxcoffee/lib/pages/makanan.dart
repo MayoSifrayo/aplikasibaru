2d52e43a8559eb38794fbc4739f2a4601d40e253
MayoSifrayo <92716573+MayoSifrayo@users.noreply.github.com>
2023-03-20 17:17:45 +0900

    'tes1'

---
+++ fikrmaxcoffee/lib/pages/makanan.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Makanan extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Makanan', style: TextStyle(color: Colors.black)), actions: [], backgroundColor: Colors.white,
+      ),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [],
+      ),
+    ));
+  }
+}
---
+++ fikrmaxcoffee/lib/pages/merchandise.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Merchandise extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Minuman', style: TextStyle(color: Colors.black)), actions: [], backgroundColor: Colors.white,
+      ),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 450,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white))),
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 450,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white))),
+          Container(
+              width: MediaQuery.of(context).size.width,
+              height: 450,
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.white))),
+        ],
+      ),
+     ));
+  }
+}
